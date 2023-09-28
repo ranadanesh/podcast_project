17944f1b660df6d477c82354ac52ed8459f92dd6
ranadanesh <ranadanesh.79@gmail.com>
2023-09-29 02:04:10 +0330

    feat: added path for episode detail

--- podcast/urls.py
+++ podcast/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .parser import parserview
-from .views import RssListView, EpisodeListView, RssDetailView
-#EpisodeDetailView
+from .views import RssListView, EpisodeListView, RssDetailView, EpisodeDetailView
 
 urlpatterns = [
     path('parse/', parserview),
     path('rss/', RssListView.as_view(), name='rss'),
     path('episode/', EpisodeListView.as_view(), name='episode'),
-    path('rss/<int:pk>/', RssDetailView.as_view(), name='rssdetail'),
+    path('rss/<int:pk>/', RssDetailView.as_view(), name='rss-detail'),
+    path('episode/<int:pk>/', EpisodeListView.as_view(), name='episode-detail'),
 ]
