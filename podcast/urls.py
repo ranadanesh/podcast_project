ca7502470521f9a3458a5c67678e61de7e69403e
ranadanesh <ranadanesh.79@gmail.com>
2023-10-12 21:00:45 +0330

    feat: updated urls

--- podcast/urls.py
+++ podcast/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .parser import parserview
-from .views import RssListView, EpisodeListView, RssDetailView, EpisodeDetailView
+from .views import RssListView, EpisodeListView, RssDetailView, EpisodeDetailView, ParseUrl
 
 urlpatterns = [
-    path('parse/', parserview),
+    path('parse/', ParseUrl.as_view(), name='parse'),
     path('rss/', RssListView.as_view(), name='rss'),
     path('episode/', EpisodeListView.as_view(), name='episode'),
     path('rss/<int:pk>/', RssDetailView.as_view(), name='rss-detail'),
