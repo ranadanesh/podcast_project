75358d896b9a509f28813aac3c5f20aeb088818a
ranadanesh <ranadanesh.79@gmail.com>
2023-10-12 21:05:20 +0330

    feat: write CustomJwtAuthentication class

--- users/authentication.py
+++ users/authentication.py
@@ -36,6 +36,46 @@ def decode_jwt(token):
     return payload
 
 
+class CustomJwtAuthentication(authentication.BaseAuthentication):
+    authentication_header_prefix = 'Token'
+    authentication_header_name = 'Authorization'
+
+    @staticmethod
+    def get_payload_from_refresh_token(refresh_token):
+        try:
+            payload = decode_jwt(refresh_token)
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise exceptions.PermissionDenied(
+                'Expired refresh token, please login again.')
+        except Exception as e:
+            raise exceptions.NotAcceptable(str(e))
+
+    @staticmethod
+    def get_user_from_payload(payload):
+
+        user_id = payload.get('id')
+        if user_id is None:
+            raise exceptions.NotFound("User Not Found Error")
+
+        try:
+            user = user_model.objects.get(id=user_id)
+            return user
+        except:
+            raise exceptions.NotFound("User Not Found ERROR")
+
+    def authenticate(self, request):
+
+        access_token = request.headers.get('Authorization')
+
+        payload = self.get_payload_from_refresh_token(access_token)
+        user = self.get_user_from_payload(payload)
+
+        return user, payload
+
+
+
+
 
 
 
