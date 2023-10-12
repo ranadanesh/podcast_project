f9207bf637b4aa45e90b5f335a5964a2ff8298b8
ranadanesh <ranadanesh.79@gmail.com>
2023-10-12 23:57:12 +0330

    feat: write celery beat for update podcast every hour

--- config/celery.py
+++ config/celery.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
-
+from celery.schedules import crontab
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('config')
@@ -14,6 +14,10 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+app.conf.beat_schedule = {
+    'update_podcast_every_hour': {
+        'task': 'podcast.tasks.update_podcast_task',
+        'schedule': crontab(hour="*/1")  # */1 -> every hour,minutes = */1 -> every minutes
+    }
+
+}
