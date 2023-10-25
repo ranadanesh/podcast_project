29cac71933d6e41cd578f8d35401bfcce9b0184d
ranadanesh <ranadanesh.79@gmail.com>
2023-10-25 19:55:25 +0330

    feat: added rabbitMQ to docker-compose.yml

--- users/managers.py
+++ users/managers.py
@@ -4,17 +4,17 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, username, email, password, **extra_fields):
 
         if not email:
             raise ValueError(_("The Email must be set"))
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         extra_fields.setdefault('is_superuser', False)
         user.save()
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, username, email, password=None, **extra_fields):
 
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
@@ -24,4 +24,4 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_("Superuser must have is_staff=True."))
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, email, password, **extra_fields)
