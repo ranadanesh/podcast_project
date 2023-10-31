61f03620c9e3d6fc09f2b57147802d44b82e634e
ranadanesh <ranadanesh.79@gmail.com>
2023-10-31 11:47:07 +0330

    feat: imported pika

--- users/consumer.py
+++ users/consumer.py
@@ -1,4 +1,7 @@
 import json
+
+import pika
+
 from .models import Notification, CustomUser
 
 
@@ -6,6 +9,5 @@ def login_callback(ch, method, properties, body):
     data = json.loads(body)
     user = CustomUser.objects.get(data['email'])
     Notification.objects.create(message=body['message'], user=user)
-
 
 
