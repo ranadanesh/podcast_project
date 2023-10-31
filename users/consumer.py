b13fcd0ed5f79fb838eba98b016c1b3653bd1453
ranadanesh <ranadanesh.79@gmail.com>
2023-10-31 11:46:25 +0330

    feat: created loging callback

---
+++ users/consumer.py
@@ -0,0 +1,11 @@
+import json
+from .models import Notification, CustomUser
+
+
+def login_callback(ch, method, properties, body):
+    data = json.loads(body)
+    user = CustomUser.objects.get(data['email'])
+    Notification.objects.create(message=body['message'], user=user)
+
+
+
