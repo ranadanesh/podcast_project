7c93c385604a461dddd41e1cedc5662af2dbb985
ranadanesh <ranadanesh.79@gmail.com>
2023-10-31 11:47:48 +0330

    feat: created login consumer

--- users/consumer.py
+++ users/consumer.py
@@ -11,3 +11,14 @@ def login_callback(ch, method, properties, body):
     Notification.objects.create(message=body['message'], user=user)
 
 
+def login_consume():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='login')
+    channel.basic_consume(queue='login', on_message_callback=login_callback)
+
+    channel.start_consuming()
+
+
