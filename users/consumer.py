3c4e05f069837d9ce849e31fcbcdee928158f368
ranadanesh <ranadanesh.79@gmail.com>
2023-10-31 11:48:31 +0330

    feat: created register consumer

--- users/consumer.py
+++ users/consumer.py
@@ -27,3 +27,13 @@ def register_callback(ch, method, properties, body):
     user = CustomUser.objects.get(data['email'])
     Notification.objects.create(message=body['message'], user=user)
 
+
+def register_consume():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='register')
+    channel.basic_consume(queue='register', on_message_callback=login_callback)
+
+    channel.start_consuming()
