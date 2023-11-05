78e5642c011df3267300d32d2bf994a0e5eed86a
ranadanesh <ranadanesh.79@gmail.com>
2023-11-05 17:35:44 +0330

    fix: fix url

--- users/publisher.py
+++ users/publisher.py
@@ -4,7 +4,7 @@ import pika
 
 def publish(queue, body):
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
+        pika.ConnectionParameters('amqp://guest:guest@rabbitmq:5672/%2F'))
     channel = connection.channel()
     channel.queue_declare(queue=queue)
     channel.basic_publish(exchange='', routing_key=queue, body=json.dumps(body))
